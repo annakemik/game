74d8ccd0ca0b190386849eda1b6f7709551e32e9
Anna Zubkova <annakemik@gmail.com>
2023-11-21 21:25:15 -0500

    Lesson 6 extra

--- script.js
+++ script.js
@@ -6,21 +6,31 @@ const isNumber = function (num) {
 
 const game = function () {
   let numberToGuess = Math.floor(Math.random() * 100);
+  let tryNumber = 10;
+  console.log(numberToGuess);
 
   function guess(userNumber) {
     if (userNumber === null) {
       alert("Игра окончена");
       return;
+    } else if (tryNumber < 1) {
+      const answer = prompt("Попытки закончились. Хотите сыграть ещё?");
+      answer === null ? alert("До свидания") : game();
     } else if (!isNumber(userNumber) || userNumber > 100 || userNumber < 1) {
       alert("Нужно вводить число от 1 до 100!");
       guess(prompt("Введи число от 1 до 100"));
     }
 
-    userNumber == numberToGuess
-      ? alert("Поздравляю, вы угадали!")
-      : userNumber < numberToGuess
-      ? guess(prompt("Загаданное число больше"))
-      : guess(prompt("Загаданное число меньше"));
+    if (userNumber == numberToGuess) {
+      const answer = prompt("Поздравляю, вы угадали! Хотели бы сыграть ещё?");
+      answer === null ? alert("До свидания") : game();
+    } else if (userNumber < numberToGuess) {
+      tryNumber--;
+      guess(prompt("Загаданное число больше"));
+    } else if (userNumber > numberToGuess) {
+      tryNumber--;
+      guess(prompt("Загаданное число меньше"));
+    }
   }
 
   guess(prompt("Угадай число от 1 до 100"));

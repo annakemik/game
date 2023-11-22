a89344eb441f955cc2d3ed816ea9c8e9a9937370
Anna Zubkova <annakemik@gmail.com>
2023-11-22 17:31:26 -0500

    Number of tires is now 10, added return statement

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const isNumber = function (num) {
 
 const game = function () {
   let numberToGuess = Math.floor(Math.random() * 100);
-  let tryNumber = 10;
+  let tryNumber = 9;
   console.log(numberToGuess);
 
   function guess(userNumber) {
@@ -14,16 +14,18 @@ const game = function () {
       alert("Игра окончена");
       return;
     } else if (tryNumber < 1) {
-      const answer = prompt("Попытки закончились. Хотите сыграть ещё?");
-      answer === null ? alert("До свидания") : game();
+      const answer = confirm("Попытки закончились. Хотите сыграть ещё?");
+      answer ? game() : alert("До свидания");
+      return;
     } else if (!isNumber(userNumber) || userNumber > 100 || userNumber < 1) {
       alert("Нужно вводить число от 1 до 100!");
       guess(prompt("Введи число от 1 до 100"));
     }
 
     if (userNumber == numberToGuess) {
-      const answer = prompt("Поздравляю, вы угадали! Хотели бы сыграть ещё?");
-      answer === null ? alert("До свидания") : game();
+      const answer = confirm("Поздравляю, вы угадали! Хотели бы сыграть ещё?");
+      answer ? game() : alert("До свидания");
+      return;
     } else if (userNumber < numberToGuess) {
       tryNumber--;
       guess(prompt("Загаданное число больше"));
